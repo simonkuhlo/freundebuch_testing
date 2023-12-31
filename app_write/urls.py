ab75d7c3a26362f4b7e05327a0b9cf7f91243b23
Simon <siga241@gmail.com>
2023-12-31 14:30:56 +0100

    add urls

--- app_write/urls.py
+++ app_write/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('home', views.home, name = "home"),
+    path('home', views.views.home, name = "home"),
+    path('new_entry/', views.new_entry.select_interview, name = "select_interview"),
+    path('new_entry/interview/<str:pk>', views.new_entry.interview, name = "new_entry"),
 ]
