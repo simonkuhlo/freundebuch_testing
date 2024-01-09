6ecc16c8887d9c613a12e26513f2cc449d926256
Simon <siga241@gmail.com>
2024-01-09 11:23:05 +0100

    optimize urls

--- app_write/urls.py
+++ app_write/urls.py
@@ -7,7 +7,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.views.home, name = "home"),
+    #start a new entry by selecting the language
     path('new_entry/', views.selects.select_language, name = "new_entry"),
-    path('new_entry/<str:language>', views.selects.select_interview, name = "new_entry"),
-    path('new_entry/interview/<str:language>+<str:interview>', views.new_entry.interview, name = "new_entry"),
+    #Interview form
+    path('new_entry/interview/<str:language>+<str:interview_id>', views.new_entry.interview, name = "new_entry"),
+    
+    
+    
+    #select the interview to follow
+    #path('new_entry/<str:language>', views.selects.select_interview, name = "new_entry"),
 ]
