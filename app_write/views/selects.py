0899035f8fddfdcf3905014570a3899b886d2207
Simon <siga241@gmail.com>
2024-01-11 12:10:45 +0100

    optimize form assignment

--- app_write/views/selects.py
+++ app_write/views/selects.py
@@ -6,6 +6,33 @@ from .. import forms
     
 
 # Create your views here.
+
+
+#select a Language
+def select_language(request):
+    form = forms.select_language(request.POST or None)
+    if request.method == 'POST':
+        
+        if form.is_valid():
+            language = form.cleaned_data['field']
+            # [!] Needs improvement. '+3' stands for Interview with ID 3. Make more dynamic for future.
+            return redirect(f'/edit/new_entry/interview/{language}+3')
+        else:
+            return redirect(f'/edit/error')
+
+    ctx = {
+            "form" : form,
+            #select_name gets displayed in Html template
+            "select_name" : "Language"
+        }
+    return render(request, 'app_write/select.html', ctx)
+
+
+
+
+
+
+#unused, currently only 1 interview for everyone
 def select_interview(request, language):
     if request.method == 'POST':
         form = forms.select_interview(request.POST)
@@ -21,18 +48,3 @@ def select_interview(request, language):
             }
     return render(request, 'app_write/select.html', ctx)
 
-
-def select_language(request):
-    if request.method == 'POST':
-        form = forms.select_language(request.POST)
-        if form.is_valid():
-            language = form.cleaned_data['field']
-            return redirect(f'/edit/new_entry/interview/{language}+3')
-        else:
-            return redirect(f'/edit/error')
-    form = forms.select_language()
-    ctx = {
-            "form" : form,
-            "select_name" : "Language"
-        }
-    return render(request, 'app_write/select.html', ctx)
