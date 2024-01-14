5da190f333346db193c60fbc1e8b849e232ff5e6
Simon <siga241@gmail.com>
2024-01-14 22:36:08 +0100

    update entry preview

--- app_read/views/listviews.py
+++ app_read/views/listviews.py
@@ -7,17 +7,12 @@ from main import models
 
 def home(request):
     all_entries = models.Entry.objects.all().order_by('created')
-    entry_dict = {}
+    entry_list = []
     i = 0
     for entry in all_entries:
-        bg_color = models.Answer.objects.filter(question__name="bg_color", entry=entry.id).first().answer_color
-        entry_dict[i] = {
-            'entry' : entry,
-            'bg_color' : bg_color
-        }
-        i += 1
+        entry_list.append(entry)
     ctx = {
-        "all_entries" : entry_dict
+        "all_entries" : entry_list
     }
 
     return render(request, 'app_read/home.html', ctx)
