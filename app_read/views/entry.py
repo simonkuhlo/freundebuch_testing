dcdd43a1abc978f33e0bf1865cc87c0bb86152e3
Simon <siga241@gmail.com>
2024-01-15 23:01:42 +0100

    simple interview display

--- app_read/views/entry.py
+++ app_read/views/entry.py
@@ -7,10 +7,34 @@ def main(request, entryid):
     language = entry.language
     all_answers = models.Answer.objects.filter(entry = entryid)
     
+    #normal questions and answers
+    item_dict = {} 
     for answer in all_answers:
+        
         question = models.Question.objects.get(id = answer.question.id)
-        print(f"Question: {question}\nAnswer: {answer}")
-    
-    ctx = { "all_answers" : all_answers}
+        if question.special == True:
+            continue
+
+        if question.type not in ["answer_text", "answer_longtext"]:
+            continue
+ 
+        match language:
+            case "de":
+                question_value = question.value_de
+            case "en":
+                question_value = question.value_en
+        
+        answer_text = answer.answer_text    
+
+        item_info = {
+            "question" : question_value,
+            "answer" : answer_text
+        }
+            
+        item_dict[question.sort_id] = item_info
+
+    item_dict = dict(sorted(item_dict.items()))
+    print(item_dict)
+    ctx = { "item_dict" : item_dict}
     
     return render(request, 'app_read/view_entry.html', ctx)
